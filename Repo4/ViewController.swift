303f4ec0c015a97d545d0dede790440f866e360d
Denis Skuratovich <den.sqrt@gmail.com>
2023-02-28 21:36:40 +0300

    add byebye

--- Repo4/ViewController.swift
+++ Repo4/ViewController.swift
@@ -9,7 +9,7 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    let greeting = "Hello!"
+    let greeting = "ByeBye!"
     
     override func viewDidLoad() {
         super.viewDidLoad()
